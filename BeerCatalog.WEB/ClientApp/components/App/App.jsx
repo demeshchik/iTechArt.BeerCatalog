193d49dfc8ee6b2d89c8f816a4db5a0d13fa3208
a.dziameshchyk <alexanderdemeshchik@outlookc.om>
2017-11-13 15:31:55 +0300

    Initial commit with server

---
+++ BeerCatalog.WEB/ClientApp/components/App/App.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable arrow-body-style,indent */
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Header from '../Header/Header';
+import Catalog from '../../containers/Catalog/Catalog';
+
+import * as grid from '../../grid.css';
+import * as styles from './app.css';
+
+const App = () => {
+    return (
+		<div className={styles.app}>
+              <Header />
+              <main className={grid.container}>
+                  <Switch>
+                      <Route exact path="/fave" component={Catalog} />
+                      <Route path="/" component={Catalog} />
+                  </Switch>
+              </main>
+		</div>
+	);
+};
+
+export default App;
---
+++ BeerCatalog.WEB/ClientApp/main.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer from './reducers/rootReducer';
+
+import App from './components/App/App';
+
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunkMiddleware)));
+
+function renderApp() {
+    ReactDOM.render(
+        <AppContainer>
+            <Provider store={store}>
+		        <BrowserRouter>
+			        <App />
+		        </BrowserRouter>
+	        </Provider>
+        </AppContainer>,
+        document.getElementById('react-app'),
+    );
+}
+
+renderApp();
+
+if (module.hot) {
+    module.hot.accept('./components/App/App', () => {
+        renderApp();
+    });
+}
